f3162c635dcbc211c94712f8e05455c84870efce
Samuel <samwkirby@gmail.com>
2022-09-22 17:38:31 -0500

    Exercise 1.1 completed

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -9,17 +9,39 @@ const App = () => {
 
   return (
     <div>
-      <h1>{course}</h1>
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
-      <p>Number of exercises {exercises1 + exercises2 + exercises3} </p>
+
+      <Header course={course}/>
+
+      <Content part={part1} exercises={exercises1}/>
+      <Content part={part2} exercises={exercises2}/>
+      <Content part={part3} exercises={exercises3}/>
+
+      <Total addend1={exercises1} addend2={exercises2} addend3={exercises3}/>
+
     </div>
   )
 }
+
+const Header = (props) => {
+  return (
+      <h1>{props.course}</h1>
+  )
+
+}
+
+const Content = (props) => {
+  return (
+    <p>
+      {props.part} {props.exercises}
+    </p>
+  )
+}
+
+const Total = (props) => {
+  var sum = props.addend1 + props.addend2 + props.addend3;
+  return (
+    <p>Number of exercises {sum}</p>
+  )
+}
+
+export default App
